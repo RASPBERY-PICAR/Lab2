692f0bb4345e2aaa38fe12803808f7323c042475
keyis2 <keyis2@illinois.edu>
2022-03-10 00:55:15 -0600

    v0.5.1

--- frontend_tutorial/camera_client.py
+++ frontend_tutorial/camera_client.py
@@ -15,6 +15,8 @@ PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
 
 sign = b'00'
 
+#
+
 
 def send_f():
     global sign
@@ -39,43 +41,43 @@ def send_f():
 
 def recv_st():
     global sign
-    stream_thread = threading.Thread(
-        target=send_f, name='Thread', daemon=True)
-    stream_thread.start()
+    # stream_thread = threading.Thread(
+    #     target=send_f, name='Thread', daemon=True)
+    # stream_thread.start()
 
-    while 1:
-        if sign == b'11':
-            break
-        elif sign == b'00':
-            continue
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
-            client_socket.connect((HOST, PORT))
-            connection = client_socket.makefile('rb')
-            try:
-                print("Streaming...Press 'q' to exit")
-                while True:
-                    # Read the length of the image as a 32-bit unsigned int. If the
-                    # length is zero, quit the loop
-                    image_len = struct.unpack(
-                        '<L', connection.read(struct.calcsize('<L')))[0]
-                    if not image_len:
-                        break
-                    # Construct a stream to hold the image data and read the image
-                    # data from the connection
-                    image_stream = io.BytesIO()
-                    image_stream.write(connection.read(image_len))
-                    # Rewind the stream, open it as an image with PIL and do some
-                    # processing on it
-                    image_stream.seek(0)
-                    image = np.array(Image.open(image_stream))
-                    cv2.imshow('image', image)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
+    # while 1:
+    #     if sign == b'11':
+    #         break
+    #     elif sign == b'00':
+    #         continue
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        client_socket.connect((HOST, PORT))
+        connection = client_socket.makefile('rb')
+        try:
+            print("Streaming...Press 'q' to exit")
+            while True:
+                # Read the length of the image as a 32-bit unsigned int. If the
+                # length is zero, quit the loop
+                image_len = struct.unpack(
+                    '<L', connection.read(struct.calcsize('<L')))[0]
+                if not image_len:
+                    break
+                # Construct a stream to hold the image data and read the image
+                # data from the connection
+                image_stream = io.BytesIO()
+                image_stream.write(connection.read(image_len))
+                # Rewind the stream, open it as an image with PIL and do some
+                # processing on it
+                image_stream.seek(0)
+                image = np.array(Image.open(image_stream))
+                cv2.imshow('image', image)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
 
-            finally:
-                # cv2.destroyAllWindows()
-                connection.close()
-                client_socket.close()
+        finally:
+            # cv2.destroyAllWindows()
+            connection.close()
+            client_socket.close()
 
 
 if __name__ == "__main__":
