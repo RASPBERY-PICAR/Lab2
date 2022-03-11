ac0ae229016fbc4ca2829cfff4ff50b44731fec6
RASPBERY-PICAR <99221530+RASPBERY-PICAR@users.noreply.github.com>
2022-03-11 10:55:11 -0600

    bluetooth

---
+++ index_bt.js
@@ -0,0 +1,71 @@
+//document.onkeydown = updateKey;
+//document.onkeyup = resetKey;
+//const bluetooth = require('node-bluetooth');
+ 
+// create bluetooth device instance
+// const device = new bluetooth.DeviceINQ();
+//var address = "E4:5F:01:42:E0:84";
+
+// function clientbt(){
+  
+//   const bluetooth = require('node-bluetooth');
+ 
+//   // create bluetooth device instance
+//   // const device = new bluetooth.DeviceINQ();
+//   var address = "E4:5F:01:42:E0:84";
+// // make bluetooth connect to remote device
+//    var input = document.getElementById("message").value;
+//   bluetooth.connect(address, 1, function(err, connection){
+//     if(err) return console.error(err);
+//     connection.write(new Buffer('${input}\r\n', 'utf-8'), () => {
+//       console.log("wrote");
+//     });
+//     // connection.write(`${input}\r\n`);
+//     // connection.on('data', (buffer) => {
+//     //   console.log('received message:', buffer.toString());
+//     // });
+
+//     //get the data from the server
+//     connection.on('data', (buffer) => {
+//       console.log('received message:', buffer.toString());
+//         var s_list = buffer.toString().split(",");
+//         var b_status = s_list[0];
+//         var t_status = s_list[1];
+//         document.getElementById("battery").innerHTML = b_status;
+//         document.getElementById("temperature").innerHTML = t_status;
+//         console.log(b_status,t_status);
+//     });
+    
+//   });
+// }
+
+
+function clientbt(){
+  
+var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
+ 
+  // create bluetooth device instance
+  // const device = new bluetooth.DeviceINQ();
+var address = "E4:5F:01:42:E0:84";
+// make bluetooth connect to remote device
+   var input = document.getElementById("message").value;   
+  btSerial.connect(address, 1, function() {
+    console.log('connected');
+  
+    btSerial.write(Buffer.from('${input}\r\n', 'utf-8'), function(err, bytesWritten) {
+      if (err) console.log(err);
+    });
+
+    btSerial.on('data', function(buffer) {
+      // console.log(buffer.toString('utf-8'));
+      var s_list = buffer.toString().split(",");
+        var b_status = s_list[0];
+        var t_status = s_list[1];
+        document.getElementById("battery").innerHTML = b_status;
+        document.getElementById("temperature").innerHTML = t_status;
+        console.log(b_status,t_status);
+    });
+    
+  });
+  btSerial.close();
+}   
