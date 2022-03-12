e4df4d0796932aca0bd3d27f9cc177364f8231ec
lhn <hainal2@illinois.edu>
2022-03-11 22:07:12 -0800

    serial-port-bluetooth

---
+++ bt_serverlhn.py
@@ -0,0 +1,42 @@
+import bluetooth
+import socket
+import picar_4wd as picar
+from time import sleep
+import helper_functions as hf
+
+hostMACAddress = "E4:5F:01:42:E0:84" # The address of Raspberry PI Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 1
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+print("listening on port ", port)
+try:     
+        
+        while 1:
+            print("Running")
+            client, clientInfo = s.accept()
+            data = client.recv(1024)
+            
+            print(data)
+
+            if data == b"Begin\r\n":
+                #sleep(1)
+                status = picar.pi_read()
+                battery_status = status['battery']
+                cpu_temp = status['cpu_temperature']
+                data2 = (str(battery_status) + ',' + str(cpu_temp)).encode('utf_8')
+                print(battery_status,cpu_temp)
+                #data2 = ('1,2').encode('utf_8')
+                print(data2)
+                client.send(data2)
+            else:
+                data3 = (' , ').encode('utf_8')
+                client.send(data3)
+            #sleep(1)
+except: 
+    print("Closing socket")
+    client.close()
+    s.close()
+
