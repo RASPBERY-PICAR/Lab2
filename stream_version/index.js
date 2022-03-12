ab2227bc6a21c1e08d1686bc36c5222508a5a8cd
keyis2 <keyis2@illinois.edu>
2022-03-12 15:08:50 -0600

    v0.5.3

--- stream_version/index.js
+++ stream_version/index.js
@@ -11,11 +11,11 @@ function connection_update(){
     console.log('change connection status');
     if (document.getElementById("connection").value == "ON") {
         document.getElementById("connection").value = "OFF";
-        disconnect_bt()
+        disconnect_bt();
         document.getElementById("connection").innerHTML = "OFF";
     } else {
-        document.getElementById("connection").value = "ON"
-        connect_bt()
+        document.getElementById("connection").value = "ON";
+        connect_bt();
         document.getElementById("connection").innerHTML = "ON";
     }
 }
@@ -39,11 +39,11 @@ function data_update(){
     console.log('update data');
     if (document.getElementById("update").value == "ON") {
         document.getElementById("update").value = "OFF";
-        clearInterval(data_flag)
-        data_flag=null
+        clearInterval(data_flag);
+        data_flag=null;
         document.getElementById("update").innerHTML = "OFF";
     } else {
-        document.getElementById("update").value = "ON"
+        document.getElementById("update").value = "ON";
         data_flag=setInterval(function(){polling_data();}, 1000);
         document.getElementById("update").innerHTML = "ON";
     }
@@ -77,7 +77,7 @@ function stream_update(){
         send_cmd('stm_ed\r\n');
         document.getElementById("stream").innerHTML = "OFF";
     } else {
-        document.getElementById("stream").value = "ON"
+        document.getElementById("stream").value = "ON";
         send_cmd('stm_st\r\n');
         document.getElementById("stream").innerHTML = "ON";
     }    
@@ -105,7 +105,7 @@ function period_up(e){
 function period_reset(e){
     if (key_flag != null) {
         clearInterval(key_flag);
-        key_flag=null
+        key_flag=null;
     }
     resetKey(e);
 }
